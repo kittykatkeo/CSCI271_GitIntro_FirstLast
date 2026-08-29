235c8ad79b4c4cf714d7af77fe50e62b5842c7db
kittykatkeo <keo.emily95@gmail.com>
2026-08-29 02:42:13 -0700

    Update header info

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,6 @@
 // CSCI 27X - GitHub Upload Practice
-// Name: YOUR NAME
-// Section: YOUR SECTION
+// Name: EMILY KEO
+// Section: Problem Solving/Programming 1
 
 #include <iostream>
 using namespace std;
